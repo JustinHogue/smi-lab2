15922a68b9267c3b791055ffcc97dc79bb2a0ecf
JustinHogue <justin.hogue.2@ulaval.ca>
2021-09-16 09:24:50 -0400

    added second blinking lights

--- src/main.c
+++ src/main.c
@@ -5,9 +5,8 @@
 #define LED_ORANGE			(BIT13)
 #define LED_ROUGE			(BIT14)
 #define LED_BLEUE			(BIT15)
-#define SysTick_LOAD_RELOAD ((u32)0x00CD1400)//¼ of processor frequency to get an interruption every 250ms
+#define SysTick_LOAD_RELOAD ((u32)0x00668A00)//TODO: Find frequency to get an interruption every 0.5ms with the if
 
-volatile uint32_t tick = 0;
 volatile uint32_t decountForSeconds = 0;
 volatile uint32_t decountForMilliSeconds = 0;
 
@@ -34,9 +33,8 @@ void configureSysTick(){
 
 void configureTIM2(){
 	RCC->APB1ENR |= BIT0; //TIM2 clock enable
-	// TODO: Determine Prescaler and Auto-Reload values!
-	//TIM2->PSC = ???;
-	TIM2->ARR = 4; //Needs to be 0.5 ms
+	TIM2->PSC = 2*(SystemCoreClock/4)/10000-1; //Base of 10 kHz
+	TIM2->ARR = 5000-1; //Needs to be 0.5 sec to work with the if conditions
 	TIM2->DIER |= BIT0; //Update interrupt enable
 	NVIC->ISER[0]=BIT28; //Enable TIM2 interrupt
 	TIM2->CR1 |= BIT0; //TIM2 counter enable
@@ -44,28 +42,28 @@ void configureTIM2(){
 
 void TIM2_IRQHandler(){
 	if (decountForMilliSeconds != 3) {
-		GPIOD->ODR |= LED_ORANGE;
-		GPIOD->ODR |= LED_ROUGE;
+		GPIOD->ODR |= LED_VERTE;
+		GPIOD->ODR |= LED_BLEUE;
 		decountForMilliSeconds++;
 	} else {
-		GPIOD->ODR &= ~(LED_ORANGE);
-		GPIOD->ODR &= ~(LED_ROUGE);
+		GPIOD->ODR &= ~(LED_VERTE);
+		GPIOD->ODR &= ~(LED_BLEUE);
 		decountForMilliSeconds = 0;
 	}
+	TIM2->SR &= ~BIT0;
+	TIM2->CR1 |= BIT0; //TIM2 counter enable
 }
 
 
 void SysTick_Handler(void)
 {
-	if (decountForSeconds == 7) {
+	if (decountForSeconds == 3) {
+		GPIOD->ODR &= ~(LED_ORANGE);
+		GPIOD->ODR &= ~(LED_ROUGE);
 		decountForSeconds = 0;
-	} else if (decountForSeconds == 6){
-		GPIOD->ODR &= ~(LED_VERTE);
-		GPIOD->ODR &= ~(LED_BLEUE);
-		decountForSeconds++;
 	} else {
-		GPIOD->ODR |= LED_VERTE;
-		GPIOD->ODR |= LED_BLEUE;
+		GPIOD->ODR |= LED_ORANGE;
+		GPIOD->ODR |= LED_ROUGE;
 		decountForSeconds++;
 	}
 }
